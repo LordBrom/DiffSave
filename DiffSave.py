6660e0cfa71e319f0efa742a95241c40d7980ec3
LordBrom <mills.nate@gmail.com>
2017-05-24 11:18:34 -0700

    Add files via upload

--- DiffSave.py
+++ DiffSave.py
@@ -9,21 +9,37 @@ def ds_settings():
 	return sublime.load_settings( 'Preferences.sublime-settings' )
 
 class dsController():
-	def get_commit_scope(self):
-		s = sublime.load_settings('Preferences.sublime-settings')
-		
+	def get_view_contents( self, view ):
+		selection = sublime.Region( 0, view.size() )
+		content = view.substr( selection )
+		return content
 
 
-
-
-class dsTestCommand(sublime_plugin.TextCommand, svnController):
+class dsDiffSaveCommand(sublime_plugin.TextCommand, dsController):
 	def run(self, edit):
-		print('starting test command')
 
+		workingView = sublime.active_window().active_view()
+		workingCopy = self.get_view_contents( workingView )
+		filePath = workingView.file_name()
 
+		linesA = workingCopy.splitlines( False )
+		fh = open(filePath)
+		linesB = [line.rstrip() for line in fh.readlines()]
+		fh.close()
 
+		diff = difflib.ndiff( linesA, linesB, linejunk = None, charjunk = None )
 
+		sublime.active_window().run_command( 'new_window' )
+		win = sublime.active_window()
+		win.set_layout( { "cols": [0.0, 1.0], "rows": [0.0, 1.0], "cells": [[0, 0, 1, 1], [0, 0, 1, 1]] } )
 
-		print('ending test command')
+		if ds_settings().get( 'toggle_sidebar', False ):
+			win.run_command( 'toggle_side_bar' )
+		if ds_settings().get( 'toggle_menu', False ):
+			win.run_command( 'toggle_menu' )
 
+		newView = win.active_view()
+		newView.set_scratch(1)
+		newView.set_syntax_file("Packages/Diff/Diff.tmLanguage");
 
+		newView.insert(edit, 0, '\n'.join( diff ))
